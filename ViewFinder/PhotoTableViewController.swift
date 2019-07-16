8468e62f517c5c431d77fd7c2a659081d0a735a8
Zach Howarth <zachhowarth@gmail.com>
2019-07-16 15:40:45 -0500

    Complete Iteration3

--- ViewFinder/PhotoTableViewController.swift
+++ ViewFinder/PhotoTableViewController.swift
@@ -57,9 +57,20 @@ class PhotoTableViewController: UITableViewController {
         //         Configure the cell...
             return cell
     }
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detailSegue", sender: photos[indexPath.row])
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+                if let photoToSend = sender as? Photos
+                {
+            photoDetailView.photo = photoToSend
+        }
+        
+        
+}
+        }}}
 
     /*
     // Override to support conditional editing of the table view.
